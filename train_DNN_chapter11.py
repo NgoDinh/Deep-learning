1e8c3ee19b5de249787a5018ac997554857248b9
vu.ngo <vu93.ngo@gmail.com>
2020-04-09 17:39:44 +0700

    3th days

--- train_DNN_chapter11.py
+++ train_DNN_chapter11.py
@@ -1,7 +1,8 @@
 # why we need activation function: if without activation function, all layer can combine to 1 layer - not deep
 # backpropagation: https://dominhhai.github.io/vi/2018/04/nn-intro/#5-lan-truy%E1%BB%81n-ng%C6%B0%E1%BB%A3c-v%C3%A0-%C4%91%E1%BA%A1o-h%C3%A0m
 # Read document above before far.
-
+#
+# IN MY OPINION, JUST USE DEFAULT PARAMETER BEFORE TRY TECHNICAL BELOW, JUST USE IT WHEN YOU REALLY FACE WITH PROBLEM
 #-------------------------------------------------
 # Some step need to concern when training a DNN. I will list down details, just some bullet point to google it
 # 1. Glorot and He Initialization: the way to random initial weight for DNN
@@ -10,7 +11,7 @@
 # 3.Batch Normalization
 # 4.Gradient Clipping; I don't really understand how it work
 #===> all technical above to help DNN avoid The Vanishing/Exploding Gradients Problems
-
+#------------------------------------------------
 # 6.Reusing Pretrained Layers
 
 import pandas as pd
@@ -46,6 +47,17 @@ def main():
 	history = model_B_on_A.fit(X_train, y_train, epochs=4,
 		validation_data=(X_valid, y_valid))
 
-
 if __name__ == '__main__':
 	main()
+
+# 7.Unsupervised Pretraining: we have a lot of unlabel data, don't waste it, use it to train unsupervise
+# NN then reuse lower layer and add new layer to pretrain supervise DNN.
+# 8.Pretraining on an Auxiliary Task
+# ====> all 3 technical above are pretrain, DNN will work well when we have a lot of data, but you don't
+# so reuse other NN network had been train before. Standing on the shoulders of giants
+#------------------------------------------------
+# 9.find faster optimizer
+# 10.Learning rate scheduling, descending over time
+#====> Make DNN faster to find optimization weight
+#-----------------------------------------------
+# 11.l2 and l1 Regularization: need to clear when to use each refular (p364)
